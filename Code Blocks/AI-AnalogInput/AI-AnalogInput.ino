3255c16d40f7797cded6f8de44074160a944e2dc
steve daniels <steve.daniels@ryerson.ca>
2021-10-13 16:38:46 -0400

    update AI

--- Code Blocks/AI-AnalogInput/AI-AnalogInput.ino
+++ Code Blocks/AI-AnalogInput/AI-AnalogInput.ino
@@ -3,22 +3,16 @@
 
 // basic analogRead with photocell circuit
 
-int photocell = A0;
-int brightness = 0; 
+int sensorPin = A0;
+int state = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-   pinMode(photocell, INPUT);
-   Serial.begin(9600);
+  pinMode(sensorPin, INPUT); 
+  Serial.begin(9600);
 }
 
 void loop() {
-  
-  // put your main code here, to run repeatedly:
-  brightness = analogRead(photocell); 
-
-  Serial.println(brightness); 
-
-  delay(10); // slow it down a little 
-  
+  state = analogRead( sensorPin ); 
+  Serial.println(state); 
+  delay(50); 
 }
